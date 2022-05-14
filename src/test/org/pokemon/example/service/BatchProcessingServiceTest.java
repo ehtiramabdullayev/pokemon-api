92645152e4969e6bb7b454a28221b23aea6e9064
Ehtiram Etibarovich <ehtiramabdullayev@gmail.com>
2022-05-14 15:01:33 +0200

    -- edited readme file

--- src/test/org/pokemon/example/service/BatchProcessingServiceTest.java
+++ src/test/org/pokemon/example/service/BatchProcessingServiceTest.java
@@ -1,28 +1,12 @@
 package org.pokemon.example.service;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-
-import org.pokemon.example.parser.PokemonParser;
-import org.pokemon.example.repo.impl.PokemonRepositoryImpl;
-
-import java.io.PrintStream;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class BatchProcessingServiceTest {
     private BatchProcessingService processingService;
 
 
-    @BeforeEach
-    public void beforeEach() {
-        processingService = new BatchProcessingService(new PokemonParser(), new PokemonRepositoryImpl());
-    }
 
-    @AfterEach
+
     public void executedAfterEach() {
         processingService = null;
     }
-
 }
