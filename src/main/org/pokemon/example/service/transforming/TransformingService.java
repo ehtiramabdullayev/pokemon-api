cd3a637c3e913638977d7c0506dd3c740bb1dc7a
Ehtiram Etibarovich <ehtiramabdullayev@gmail.com>
2022-08-31 02:25:21 +0200

    re-organized the package structure

--- src/main/org/pokemon/example/service/transforming/TransformingService.java
+++
@@ -1,5 +0,0 @@
-package org.pokemon.example.service.transforming;
-
-public interface TransformingService<T> {
-    T transform(T obj);
-}
--- src/main/org/pokemon/example/service/transforming/function/TransformingFunction.java
+++
@@ -1,6 +0,0 @@
-package org.pokemon.example.service.transforming.function;
-
-public interface TransformingFunction<T> {
-    boolean couldBeApplied(T obj);
-    T apply(T obj);
-}
