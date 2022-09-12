8c0385ca6bf437410a393a8ef367d02927290925
Ehtiram Etibarovich <ehtiramabdullayev@gmail.com>
2022-09-12 21:05:29 +0200

    added test for repository

--- src/test/java/org/pokemon/example/repo/PokemonRepoTest.java
+++ src/test/java/org/pokemon/example/repo/PokemonRepoTest.java
@@ -1,28 +1,39 @@
-//package org.pokemon.example.repo;
-//
-//import org.junit.jupiter.api.Test;
-//import org.pokemon.example.model.PokemonEntity;
-//import org.pokemon.example.repo.impl.PokemonRepositoryImpl;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//class PokemonRepoTest {
-//    @Test
-//    public void test_whenTryToSaveTokenSuccess() {
-//        PokemonRepo authRepository = new PokemonRepositoryImpl();
-//        PokemonEntity entity = new PokemonEntity("name", "type", "type2", 100, 50, 55.5, 60.5, 58, 62, 70, 90, false);
-//        assertTrue(authRepository.savePokemon("1", entity));
-//    }
-//
-//    @Test
-//    public void test_whenTryGetTokenListSuccess() {
-//        PokemonRepo authRepository = new PokemonRepositoryImpl();
-//        PokemonEntity entity = new PokemonEntity("name", "type", "type2", 100, 50, 55.5, 60.5, 58, 62, 70, 90, false);
-//        PokemonEntity entity2 = new PokemonEntity("name2", "type2", "type3", 200, 50, 55.5, 60.5, 58, 62, 70, 90, false);
-//        assertTrue(authRepository.savePokemon("1", entity));
-//        assertTrue(authRepository.savePokemon("2", entity2));
-//        assertTrue(authRepository.getAllPokemonList().isPresent());
-//        assertEquals(2, authRepository.getAllPokemonList().get().size());
-//    }
-//}
+package org.pokemon.example.repo;
+
+import org.junit.jupiter.api.Test;
+import org.pokemon.example.model.PokemonEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@DataJpaTest
+class PokemonRepoTest {
+    @Autowired
+    private TestEntityManager entityManager;
+    @Autowired
+    private PokemonRepo authRepository;
+
+    @Test
+    public void test_whenTryToSaveTokenSuccess() {
+        PokemonEntity entity = new PokemonEntity("1", "name", "type", "type2", 100, 50, 55.5, 60.5, 58, 62, 70, 90, false);
+        PokemonEntity savedEntity = authRepository.save(entity);
+        assertNotNull(savedEntity);
+        assertEquals("name", savedEntity.getName());
+    }
+
+    @Test
+    public void test_whenTryGetTokenListSuccess() {
+        PokemonEntity entity = new PokemonEntity("1", "name", "type", "type2", 100, 50, 55.5, 60.5, 58, 62, 70, 90, false);
+        PokemonEntity entity2 = new PokemonEntity("2", "name2", "type2", "type3", 200, 50, 55.5, 60.5, 58, 62, 70, 90, false);
+        PokemonEntity firstSavedEntity = authRepository.save(entity);
+        PokemonEntity secondSavedEntity = authRepository.save(entity2);
+        assertNotNull(firstSavedEntity);
+        assertNotNull(secondSavedEntity);
+        assertFalse(authRepository.findAll().isEmpty());
+        assertEquals(2, authRepository.findAll().size());
+    }
+}
